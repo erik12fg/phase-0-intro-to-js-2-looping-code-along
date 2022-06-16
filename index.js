be75635da4a3386889530f3fad9778138b15838a
erik gomez <erik12fgtv@gmail.com>
2022-06-15 18:36:14 -0700

    commit tree e32efb7c91478cba4a6e7f12b52659e06bc50265

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(name, greeting) {
+    let messages = []
+    for (let i = 0; i < name.length; i++) {
+        let array = name[i]
+        let message = (`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+        messages.push(message)
+    }
+    return messages
+}
+
+const number = []
+function countDown(number) {
+    let i = 10;
+    while(i >= 0) {
+        console.log(i--)
+    }
+}
